5226a02308ec29a7f08e44732e426bee1de7a0eb
Liber Dovat <liber.dovat@gmail.com>
2016-05-10 00:36:41 -0300

    editados los iconos y agregados nuevos sitios

--- maya/js/maya.js
+++ maya/js/maya.js
@@ -24,17 +24,24 @@ OpenStreetMap_Mapnik.addTo(map);
 map.setView([17.810278,-89.373470], 7);
 
 var maya = L.icon({
-  iconUrl: './js/images/maya.png',
-  iconRetinaUrl: './js/images/mayax2.png',
-  iconSize: [69, 40],
-  iconAnchor: [69, 40],
+  iconUrl: './js/images/maya_20.png',
+  iconRetinaUrl: './js/images/maya_40.png',
+  iconSize: [35, 20],
+  iconAnchor: [35, 20],
   popupAnchor: [-16, -38],
-  shadowUrl: './js/images/marker-shadow.png',
+//   shadowUrl: './js/images/marker-shadow.png',
 });
-var icon =  {icon: maya, title: "Maya"};
+var icon = {icon: maya, title: "Maya"};
 
 // http://forums.civfanatics.com/archive/index.php/t-454630.html
 // http://www012.upp.so-net.ne.jp/klivo/majaoj/citynames.html
+// https://en.wikipedia.org/wiki/List_of_Maya_sites
+
+// https://github.com/Leaflet/Leaflet/issues/859
+
+var altun = new L.LatLng(17.76395,-88.347061);
+var altun_marker = L.marker(altun,icon).addTo(map);
+altun_marker.bindPopup("<strong>Altun Ha</strong>");
 
 var coba = new L.LatLng(20.494722,-87.736111);
 var coba_marker = L.marker(coba,icon).addTo(map);
@@ -44,6 +51,10 @@ var copan = new L.LatLng(14.84,-89.14);
 var copan_marker = L.marker(copan,icon).addTo(map);
 copan_marker.bindPopup("<strong>Copán</strong>");
 
+var cahal = new L.LatLng(17.1457,-89.0741);
+var cahal_marker = L.marker(cahal,icon).addTo(map);
+cahal_marker.bindPopup("<strong>Cahal Pech</strong>");
+
 var calakmul = new L.LatLng(18.105392,-89.810828);
 var calakmul_marker = L.marker(calakmul,icon).addTo(map);
 calakmul_marker.bindPopup("<strong>Calakmul</strong>");
@@ -52,9 +63,21 @@ var caracol = new L.LatLng(16.763889,-89.1175);
 var caracol_marker = L.marker(caracol,icon).addTo(map);
 caracol_marker.bindPopup("<strong>Caracol</strong>");
 
+var chichen = new L.LatLng(20.6829,-88.56865);
+var chichen_marker = L.marker(chichen,icon).addTo(map);
+chichen_marker.bindPopup("<strong>Chichen Itza</strong>");
+
 var cival = new L.LatLng(17.2825,-89.346667);
 var cival_marker = L.marker(cival,icon).addTo(map);
 cival_marker.bindPopup("<strong>Cival</strong>");
+
+var cerros = new L.LatLng(18.358333,-88.351944);
+var cerros_marker = L.marker(cerros,icon).addTo(map);
+cerros_marker.bindPopup("<strong>Cerros</strong>");
+
+var lamanai = new L.LatLng(17.7525,-88.654444);
+var lamanai_marker = L.marker(lamanai,icon).addTo(map);
+lamanai_marker.bindPopup("<strong>Lamanai</strong>");
 
 var mixco = new L.LatLng(14.871667,-90.664167);
 var mixco_marker = L.marker(mixco,icon).addTo(map);
@@ -63,6 +86,10 @@ mixco_marker.bindPopup("<strong>Mixco Viejo</strong>");
 var motul = new L.LatLng(17.026389,-89.901389);
 var motul_marker = L.marker(motul,icon).addTo(map);
 motul_marker.bindPopup("<strong>Motul de San José</strong>");
+
+var palenque = new L.LatLng(17.483978,-92.046328);
+var palenque_marker = L.marker(palenque,icon).addTo(map);
+palenque_marker.bindPopup("<strong>Palenque</strong>");
 
 var quirigua = new L.LatLng(15.269444,-89.040278);
 var quirigua_marker = L.marker(quirigua,icon).addTo(map);
@@ -82,12 +109,29 @@ rita_marker.bindPopup("<strong>Santa Rita Corozal</strong>");
 
 var sayil = new L.LatLng(20.178,-89.6525);
 var sayil_marker = L.marker(sayil,icon).addTo(map);
-sayil_marker.bindPopup("<strong>sayil</strong>");
+sayil_marker.bindPopup("<strong>Sayil</strong>");
 
 var seibal = new L.LatLng(16.511667,-90.061111);
 var seibal_marker = L.marker(seibal,icon,icon).addTo(map);
 seibal_marker.bindPopup("<strong>Seibal</strong>");
 
+var takalik = new L.LatLng(14.645833,-91.736111);
+var takalik_marker = L.marker(takalik,icon).addTo(map);
+takalik_marker.bindPopup("<strong>Takalik Abaj</strong>");
+
 var tikal = new L.LatLng(17.222094,-89.623614);
 var tikal_marker = L.marker(tikal,icon).addTo(map);
-tikal_marker.bindPopup("<strong>tikal</strong>");
+tikal_marker.bindPopup("<strong>Tikal</strong>");
+
+var uxmal = new L.LatLng(20.359444,-89.771389);
+var uxmal_marker = L.marker(uxmal,icon).addTo(map);
+uxmal_marker.bindPopup("<strong>Uxmal</strong>");
+
+var xunantunich = new L.LatLng(17.083889,-89.133889);
+var xunantunich_marker = L.marker(xunantunich,icon).addTo(map);
+xunantunich_marker.bindPopup("<strong>Xunantunich</strong>");
+
+
+L.easyButton( '<span class="star">&equiv;</span>', function(){
+  map.setView([17.810278,-89.373470], 7, {animate: true});
+}).addTo(map);
